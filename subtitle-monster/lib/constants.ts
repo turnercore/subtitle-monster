9d0b31902ce90d43a1ae3058f95f53f82d6439ba
Turner Monroe <me@turnercore.dev>
2023-12-29 17:08:11 +0000

    init

---
+++ subtitle-monster/lib/constants.ts
@@ -0,0 +1,12 @@
+// Exported Constants
+
+const projectName = 'subtitle-monster'
+const projectPrefix = 'sm'
+
+// See if database is shared
+const isSharedDb = process.env.SHARED_DB ? process.env.SHARED_DB : false
+const hasSharedProfiles = process.env.SHARED_PROFILES ? process.env.SHARED_PROFILES : true
+
+// Tables in DB
+export const PROFILES_TABLE = hasSharedProfiles || !isSharedDb ? 'profiles' : projectPrefix + '_profiles'
+export const SUBTITLES_TABLE = isSharedDb ? projectPrefix + '_subtitles' : 'subtitles'
